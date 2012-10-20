bcc9f6a2fc9918fa3d491fafe197928933884692
comboy <kacper.ciesla@gmail.com>
2012-10-20 12:55:46 +0200

    center all dialogs on the screen by default
    just like zenity does

--- lib/zerenity/messagedialog.rb
+++ lib/zerenity/messagedialog.rb
@@ -12,6 +12,7 @@ module Zerenity
       dialog = Gtk::MessageDialog.new(nil,Gtk::Dialog::MODAL,options[:type],Gtk::MessageDialog::BUTTONS_NONE,options[:text])
       self.build(dialog,options)
       dialog.set_title(options[:title]) if options[:title]
+      dialog.set_window_position(Gtk::Window::POS_CENTER)
       result = nil
       if options[:cancel_button]
         options[:cancel_button].signal_connect(CLICKED) do
